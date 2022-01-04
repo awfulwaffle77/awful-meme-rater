f7a18850e51726f40507fb4beed01c7015fde42c
unknown <heroidorin@gmail.com>
2022-01-05 00:20:22 +0200

    Added some OCR

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 [praw.ini documentation](https://praw.readthedocs.io/en/latest/getting_started/configuration/prawini.html#praw-ini)
 
 [submission attributes](https://praw.readthedocs.io/en/latest/code_overview/models/submission.html#submission)
+
+[install tesseract](https://nanonets.com/blog/ocr-with-tesseract/#preprocessingfortesseract)
+
+## *To do*
+- interpret score and date as factors deciding if meme is popular or not
+- train with the given data and crafted labels
--- generate_dataset.py
+++ generate_dataset.py
@@ -1,16 +1,37 @@
 import praw
 import requests
 import os
+import cv2
+import pytesseract
+import enchant
+import re
 
 RESOURCES_FOLDER = os.path.join(os.getcwd(), "res\\")
 
 
-def write_attrs(_submission, _reference_name):
-    f = open(RESOURCES_FOLDER + _reference_name + ".attrs", "w")
+def write_attrs(_submission, _reference_name, save_line):
+    f = open(RESOURCES_FOLDER + _reference_name + ".attrs", "w", encoding="utf-8")
     f.write("title=" + str(_submission.title) + "\n")
     f.write("score=" + str(_submission.score) + "\n")
     f.write("created_utc=" + str(_submission.created_utc) + "\n")
+    if save_line != "":
+        f.write("text=" + save_line + "\n")
     f.close()
+
+
+def all_words_correct(line):
+    all_blank = True
+    for word in line:
+        if word == "":
+            continue
+        if not d.check(word):
+            return False
+        else:
+            all_blank = False
+    if not all_blank:
+        return True
+    else:
+        return False
 
 
 def meets_requirements(_submission):
@@ -20,6 +41,8 @@ def meets_requirements(_submission):
 
 
 if __name__ == "__main__":
+
+    d = enchant.Dict("en_US")
 
     # cont = input("Folder " + RESOURCES_FOLDER + " will be emptied. Continue? y/N ")
     cont = "y"
@@ -50,5 +73,20 @@ if __name__ == "__main__":
         with open(RESOURCES_FOLDER + reference_name, 'wb') as handler:
             handler.write(img_data)
 
-        write_attrs(submission, reference_name_no_ext)
+            img = cv2.Canny(cv2.medianBlur(cv2.imread(RESOURCES_FOLDER + reference_name, cv2.IMREAD_GRAYSCALE), 5), 100, 200)
+            cv2.imshow("img", img)
+            cv2.waitKey(0)
+            text = pytesseract.image_to_string(img)
+
+            text_lines = text.split("\n")
+            final_line = ""
+            for line in text_lines:
+                save_line = ""
+                words = line.split(" ")
+                if all_words_correct(words):
+                    final_line += " ".join(words) + "\n"
+
+            print(final_line)
+
+        write_attrs(submission, reference_name_no_ext, final_line)
 
